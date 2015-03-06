0182c5e9879db47167cf99061bf9eb60597c5235
Randy Lai <randy.cs.lai@gmail.com>
2015-03-06 00:06:45 -0800

    add updater

---
+++ update.py
@@ -0,0 +1,9 @@
+import sublime
+
+
+def plugin_loaded():
+    settings_file = 'pm.sublime-settings'
+    settings = sublime.load_settings(settings_file)
+    if settings.get("use_machine_projects_dir", False):
+        settings.set('use_local_projects_dir', True)
+        sublime.save_settings(settings_file)
