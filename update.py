8669a1bb62a9c1bb87d019ed7c00b9a646d82f47
Randy Lai <randy.cs.lai@gmail.com>
2015-03-07 12:17:09 -0800

    earse old setting

--- update.py
+++ update.py
@@ -6,4 +6,5 @@ def plugin_loaded():
     settings = sublime.load_settings(settings_file)
     if settings.get("use_machine_projects_dir", False):
         settings.set('use_local_projects_dir', True)
+        settings.erase("use_machine_projects_dir")
         sublime.save_settings(settings_file)
