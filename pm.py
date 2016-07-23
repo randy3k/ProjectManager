e97516464ee9744c607b96171631934e992a0fea
Randy Lai <randy.cs.lai@gmail.com>
2016-07-23 17:08:41 -0400

    rename as ProjectManager

--- pm.py
+++ pm.py
@@ -7,14 +7,27 @@ import platform
 import re
 
 
-def update_settings():
+def plugin_loaded():
     t = sublime.load_settings("Project Manager.sublime-settings")
-    projects_path = t.get("projects_fpath")
-    if projects_path:
-        t.set("projects_path", projects_path)
-        sublime.save_settings("Project Manager.sublime-settings")
+    s = sublime.load_settings("project_manager.sublime-settings")
+    keys = [
+        "projects_path",
+        "use_local_projects_dir",
+        "show_open_files",
+        "show_recent_projects_first"
+    ]
+    d = {}
+    for k in keys:
+        if t.has(k) and not s.has(k):
+            d.update({k: t.get(k)})
+    for key, value in d.items():
+        s.set(key, value)
+    if d:
+        sublime.save_settings("project_manager.sublime-settings")
 
-update_settings()
+    old_file = os.path.join(sublime.packages_path(), "User", "Project Manager.sublime-settings")
+    if os.path.exists(old_file):
+        os.unlink(old_file)
 
 
 class JsonFile:
