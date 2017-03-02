88c385886d0679d5e5da572153a0dac1ae0813f3
Johannes Rappen <jrappen@users.noreply.github.com>
2017-03-02 21:32:45 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,7 @@
 __pkg_name__ = 'ProjectManager'
+
+from .fix_old_settings_file.py import *
+from .json_file import *
+from .project_manager import *
+from .text_commands import *
+from .window_commands import *
