3b8df0a2259e4c61e009a9448746e29f545bec44
Randy Lai <randy.cs.lai@gmail.com>
2017-03-21 01:08:29 -0400

    fix import

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 __pkg_name__ = 'ProjectManager'
 
-from .fix_old_settings_file.py import *
+from .fix_old_settings_file import *
 from .json_file import *
 from .project_manager import *
 from .text_commands import *
