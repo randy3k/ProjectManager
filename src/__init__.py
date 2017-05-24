36982257a1f5619ab528d4ddd8b782685b0b0236
Randy Lai <randy.cs.lai@gmail.com>
2017-05-24 00:00:33 -0400

    remove fix old settings code

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,4 @@
 __pkg_name__ = 'ProjectManager'
 
-from .fix_old_settings_file import *
-from .json_file import *
-from .project_manager import *
 from .text_commands import *
 from .window_commands import *
