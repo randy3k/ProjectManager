52e9e85329e77d386eac32dfd7144a69df919951
Val95240 <Valentin.GUILLET@isae-supaero.fr>
2022-11-12 20:46:44 +0100

    Update tests to be more complete

--- tests/test_add_and_open.py
+++ tests/test_add_and_open.py
@@ -30,31 +30,105 @@ class TestBasicFeatures(TempDirectoryTestCase, OverridePreferencesTestCase):
     def setUp(self):
         yield from self.__class__.setWindowFolder()
 
+    def get_new_window(self, wids):
+        for window in sublime.windows():
+            if window.id() not in wids:
+                return window
+
+    def get_ws_names(self):
+        wfiles = self.manager.projects_info.info[self.project_name]["workspaces"]
+        return map(lambda wfile: os.path.basename(wfile)[:-18], wfiles)
+
+    def get_wfile(self, wname):
+        for wfile in self.manager.projects_info.info[self.project_name]["workspaces"]:
+            if os.path.basename(wfile)[:-18] == wname:
+                return wfile
+
     def test_add_and_open_with_mock(self):
-        def _window_show_input_panel(wid, caption, initial_text, on_done, on_change, on_cancel):
-            sublime.set_timeout(lambda: on_done(initial_text), 100)
-            return 0
+        self.window.run_command("close_project")
 
-        with patch("sublime_api.window_show_input_panel", _window_show_input_panel):
-            self.window.run_command("project_manager", {"action": "create_project"})
-            yield lambda: self.project_name in self.manager.projects_info.info
+        # Create project
+        self.window.run_command("project_manager", {"action": "create_project",
+                                                    "value": self.project_name})
+        yield lambda: self.project_name in self.manager.projects_info.info
 
-        projects_info = self.manager.projects_info.info
+        # Open project in new window
+        wids = set(map(lambda w: w.id(), sublime.windows()))
+        self.window.run_command("project_manager", {"action": "open_project",
+                                                    "project": self.project_name})
+        yield lambda: len(sublime.windows()) == len(wids) + 1
 
-        self.assertTrue(self.project_name in projects_info)
+        # Close current window and switch to newly open project
+        self.window.run_command("close_window")
+        self.window = self.get_new_window(wids)
+        yield lambda: self.window.project_file_name() is not None
 
-        if sublime.version() >= '4000':
-            def _window_show_quick_panel(wid, items, on_done, *args, **kwargs):
-                index = next(i for i, item in enumerate(items)
-                             if item[0].startswith(self.project_name))
-                sublime.set_timeout(lambda: on_done(index), 100)
-        else:
-            def _window_show_quick_panel(wid, items, items_per_row, on_done, *args, **kwargs):
-                index = next(int(i / items_per_row) for i, item in enumerate(items)
-                             if i % items_per_row == 0 and item.startswith(self.project_name))
-                sublime.set_timeout(lambda: on_done(index), 100)
+        # Edit project
+        self.window.run_command("project_manager", {"action": "edit_project",
+                                                    "project": self.project_name})
+        pfile = self.manager.projects_info.info[self.project_name]["file"]
+        yield lambda: len(self.window.views()) > 0
+        yield lambda: self.window.views()[0].file_name() == pfile
+        self.window.run_command("close_file")
 
-        with patch("sublime_api.window_show_quick_panel", _window_show_quick_panel):
-            with patch("sublime.ok_cancel_dialog", return_value=True):
-                self.window.run_command("project_manager", {"action": "remove_project"})
-                yield lambda: self.project_name not in self.manager.projects_info.info
+        # Rename project...
+        self.window.run_command("project_manager", {"action": "rename_project",
+                                                    "project": self.project_name,
+                                                    "value": "123tmp_proj890"})
+        yield os.path.exists(self.manager.projects_info.info["123tmp_proj890"]["file"])
+
+        # ...and undo
+        self.window.run_command("project_manager", {"action": "rename_project",
+                                                    "project": "123tmp_proj890",
+                                                    "value": self.project_name})
+        yield os.path.exists(self.manager.projects_info.info[self.project_name]["file"])
+
+        # Add a new workspace
+        new_wname = "test_ws"
+        wids = set(map(lambda w: w.id(), sublime.windows()))
+        self.window.run_command("project_manager", {"action": "add_workspace",
+                                                    "project": self.project_name,
+                                                    "value": new_wname})
+        yield lambda: len(self.manager.projects_info.info[self.project_name]["workspaces"]) == 2
+        yield lambda: len(sublime.windows()) == len(wids) + 1
+
+        # Close this new workspace
+        new_ws_window = self.get_new_window(wids)
+        new_ws_window.run_command("close_window")
+        yield lambda: len(sublime.windows()) == len(wids)
+
+        # Next set of tests only seems to work locally, not on github
+
+        # if sublime.version() > '4050':      # Need to get workspace file name to work
+        #     # Switch to this workspace via `open_project` cmd
+        #     self.window.run_command("project_manager", {"action": "open_project",
+        #                                                 "workspace": new_wname})
+        #     yield lambda: self.manager.is_workspace_open(self.get_wfile(new_wname))
+
+        #     # Switch back to default workspace via `open_workspace` cmd
+        #     self.window.run_command("project_manager", {"action": "open_workspace",
+        #                                                 "workspace": self.project_name})
+        #     yield lambda: self.manager.is_workspace_open(self.get_wfile(self.project_name))
+
+        # # Rename the workspace
+        # self.window.run_command("project_manager", {"action": "rename_workspace",
+        #                                             "workspace": new_wname,
+        #                                             "value": "renamed_ws"})
+        # yield lambda: len(self.manager.projects_info.info[self.project_name]["workspaces"]) == 2
+        # new_wname = "renamed_ws"
+        # yield lambda: new_wname in self.get_ws_names()
+
+        # # Remove the renamed workspace
+        # with patch("sublime.ok_cancel_dialog", return_value=True):
+        #     self.window.run_command("project_manager", {"action": "remove_workspace",
+        #                                                 "workspace": new_wname})
+        #     yield lambda: len(self.manager.projects_info.info[self.project_name]["workspaces"]) == 1
+
+        # Remove the project
+        with patch("sublime.ok_cancel_dialog", return_value=True):
+            self.window.run_command("project_manager", {"action": "remove_project",
+                                                        "project": self.project_name})
+            yield lambda: self.project_name not in self.manager.projects_info.info
+
+        # Close testing window
+        self.window.run_command("close_window")
